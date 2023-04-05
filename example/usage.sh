13f4fcb745b99b4912727529ab5684db5504b23a
Roman Ožana <roman@ozana.cz>
2023-04-05 18:06:09 +0200

    Add function to usage

--- example/usage.sh
+++ example/usage.sh
@@ -6,3 +6,7 @@ echo "this is ${MAGENTA}magenta ${BLINK}blink${NC} text${NC}"
 
 # or you can use the functions
 echo $(gray "[INFO] " && green "This is gree test")
+
+# there can be a function that uses the colors
+function info() { echo $(gray "[INFO] " && yellow "$@"); }
+info "This is a info test"
